7bbf699cef603c765ffa278700818640210540b7
hoshico <melancholinista.yasu@gmail.com>
2021-12-06 08:42:07 +0900

    update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,3 +2,13 @@ import * as firebase from "firebaes/app";
 import "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+});
