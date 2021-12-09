30fa59c747ca1990e8404743019d575c2268d997
hoshico <melancholinista.yasu@gmail.com>
2021-12-10 08:29:24 +0900

    add App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,71 @@
-import { FormControl, List, TextField } from '@material-ui/core';
-import React, { useState, useEffect} from 'react';
-import styles from './App.module.css';
+import { FormControl, List, TextField } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import styles from "./App.module.css";
 import { db } from "./firebase";
-import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
-import { TaskItem } from './TaskItem';
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import { TaskItem } from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
 
 const App: React.FC = () => {
-  const [ tasks, setTasks ] = useState([{id: "", title: ""}]);
-  const [ input, setInput ] = useState("");
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+
+  //マテリアルUIの場合はfunctionalコンポーネントの中でuseStylesを実行する必要がある
+  const classes = useStyles();
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
-        snapshot.docs.map((doc) => ({id: doc.id, title: doc.data().title}))
-      )
-    })
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
     return () => unSub();
-  },[])
+  }, []);
 
   const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
-    db.collection("tasks").add({title: input});
+    db.collection("tasks").add({ title: input });
     setInput("");
-  }
+  };
 
   return (
-    <div className="App">
+    <div className={styles.app__root}>
       <h1>Todo App by React/Firebase</h1>
+      <br />
       <FormControl>
-        <TextField 
+        <TextField
+          className={classes.field}
           InputLabelProps={{
             shrink: true,
           }}
           label="New task ?"
           value={input}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setInput(e.target.value)
           }
         />
       </FormControl>
-      <button disabled={!input} onClick={newTask}>
-          <AddToPhotosIcon />
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
       </button>
 
-      <List>
+      <List className={classes.list}>
         {tasks.map((task) => (
-          <TaskItem key={task.id} id={task.id} title={task.title}/>
+          <TaskItem key={task.id} id={task.id} title={task.title} />
         ))}
       </List>
     </div>
   );
-}
+};
 
 export default App;
